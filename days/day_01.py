6ff1466ae18ed2d4b9a65c2e3a5a7c63f251bd64
Clement Gaboriau Couanau <clement.gaboriaucouanau@datadoghq.com>
2025-12-02 00:05:56 +0100

    [DAY-01] Improve Day 1 solution

--- days/day_01.py
+++ days/day_01.py
@@ -7,6 +7,8 @@ class Day01(Day):
     FIRST_STAR_TEST_RESULT = 3
     SECOND_STAR_TEST_RESULT = 6
 
+    DIR = {"R": 1, "L": -1}
+
     def __init__(self):
         super().__init__(self)
 
@@ -14,8 +16,8 @@ class Day01(Day):
     def password(input_value: InputParser) -> int:
         dial = 50
         pwd = 0
-        for rotation in input_value.get_iterator():
-            dial = (dial + (1 if rotation[0] == "R" else -1) * int(rotation[1:])) % 100
+        for r in input_value.get_iterator():
+            dial = (dial + Day01.DIR[r[0]] * int(r[1:])) % 100
             if dial == 0:
                 pwd += 1
         return pwd
@@ -24,22 +26,19 @@ class Day01(Day):
     def password_2(input_value: InputParser) -> int:
         dial = 50
         pwd = 0
-        for rotation in input_value.get_iterator():
-            r = int(rotation[1:])
-            pwd += r // 100
-            r = r % 100
-            if r == 0:
+        for r in input_value.get_iterator():
+            v = int(r[1:])
+            pwd += v // 100
+            v %= 100
+            if v == 0:
                 continue
             prev_dial = dial
-            dial += (1 if rotation[0] == "R" else -1) * r
-            if dial >= 100:
-                dial -= 100
-                pwd += 1
-            elif dial < 0:
-                dial += 100
+            dial += Day01.DIR[r[0]] * v
+            if dial < 0 or dial >= 100 :
                 pwd += 1 if prev_dial != 0 else 0
             elif dial == 0:
                 pwd += 1
+            dial %= 100
         return pwd
 
     def solution_first_star(
