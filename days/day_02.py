d052c168c5d1cc6a8429a7a4fe2a567010e82e2d
Clement Gaboriau Couanau <clement.gaboriaucouanau@datadoghq.com>
2025-12-06 15:56:22 +0100

    [DAY-02] Improve Day02 logic

--- days/day_02.py
+++ days/day_02.py
@@ -1,10 +1,11 @@
+import bisect
+
 from day_factory.day import Day
 from day_factory.day_utils import TestEnum
 from utils.input_parser import InputParser
 
 
 class Day02(Day):
-
     FIRST_STAR_TEST_RESULT = 1227775554
     SECOND_STAR_TEST_RESULT = 4174379265
 
@@ -13,48 +14,41 @@ class Day02(Day):
 
     @staticmethod
     def solve(input_value: InputParser) -> int:
-        ranges = next(input_value.get_iterator()).split(',')
+        ranges = next(input_value.get_iterator()).split(",")
         total = 0
         for r in ranges:
-            start, end = r.split('-')
+            start, end = r.split("-")
             if len(start) % 2 == 1:
-                a = 10 ** (len(start)//2)
+                a = 10 ** (len(start) // 2)
             else:
-                a = int(start[0:len(start)//2])
+                a = int(start[0 : len(start) // 2])
             if len(end) % 2 == 1:
-                b = 10 ** (len(end)//2) - 1
+                b = 10 ** (len(end) // 2) - 1
             else:
-                b = int(end[0:len(end)//2])
-            for v in range(a, b+1):
-                invalid_id = int(str(v)*2)
+                b = int(end[0 : len(end) // 2])
+            for v in range(a, b + 1):
+                invalid_id = int(str(v) * 2)
                 if int(start) <= invalid_id <= int(end):
                     total += invalid_id
         return total
 
     @staticmethod
     def solve_2(input_value: InputParser) -> int:
-        ranges = next(input_value.get_iterator()).split(',')
-        total = 0
-        for r in ranges:
-            start, end = r.split('-')
-            for candidate in range(int(start), int(end)+1):
-                s = str(candidate)
-                for size in range(1, len(s)//2+1):
-                    if len(s) % size != 0:
-                        continue
-                    ptr = 0
-                    is_valid = False
-                    while ptr+2*size <= len(s):
-                        if s[ptr:ptr+size] != s[ptr+size:ptr+2*size]:
-                            is_valid = True
-                            break
-                        ptr += size
-                    if not is_valid:
-                        break
-                else:
-                    continue
-                total += candidate
-        return total
+        ranges = next(input_value.get_iterator()).split(",")
+        invalid_ids = set()
+        start = sorted([int(r.split("-")[0]) for r in ranges])
+        end = sorted([int(r.split("-")[1]) for r in ranges])
+        i = 1
+        while int(str(i) * 2) <= end[-1] + 1:
+            repeat = 2
+            while int(str(i) * repeat) <= end[-1] + 1:
+                invalid_id = int(str(i) * repeat)
+                idx = bisect.bisect_left(end, invalid_id)
+                if start[idx] <= invalid_id <= end[idx]:
+                    invalid_ids.add(invalid_id)
+                repeat += 1
+            i += 1
+        return sum(invalid_ids)
 
     def solution_first_star(
         self, input_value: InputParser, input_type: TestEnum
